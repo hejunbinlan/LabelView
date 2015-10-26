5926398a5a7af9596d8b696a8f541d7e4b91f682
drakeet <drakeet.me@gmail.com>
2015-10-26 13:27:05 +0800

    Add labelView.setText() demo.

--- sample/src/main/java/me/drakeet/demo/MainActivity.java
+++ sample/src/main/java/me/drakeet/demo/MainActivity.java
@@ -2,6 +2,9 @@ package me.drakeet.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.normal_text);
-        TextView labelView = (TextView) findViewById(R.id.lableview);
+        final TextView labelView = (TextView) findViewById(R.id.lableview);
         textView.append(labelView.getText());
+
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+                labelView.setText(s);
+            }
+
+
+            @Override public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
